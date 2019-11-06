9fab76a0a03676925aa034638e4e6b5dfd2217af
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2019-11-06 08:34:54 -0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,15 +1,23 @@
 from discord.ext import commands
 
+from core import checks
+from core._color_data import ALL_COLORS
+from core.models import PermissionLevel
+
 class Say(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+        
     @commands.Cog.listener()
     async def on_message(self, message):
         print(message.content)
 
     @commands.command()
+    @checks.has_permissions(PermissionLevel.REGULAR)
     async def say(self, ctx, *, message):
+        """
+        Hey.
+        """
         await ctx.send(message)
 
 def setup(bot):
