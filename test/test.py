2cec8cc12104e533a7c33c9c767dc4d7a0965276
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2019-11-06 07:19:16 -0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,1 +1,16 @@
+from discord.ext import commands
 
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        print(message.content)
+
+    @commands.command()
+    async def say(self, ctx, *, message):
+        await ctx.send(message)
+
+def setup(bot):
+    bot.add_cog(MyCog(bot))
