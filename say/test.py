7c804673819024375e0b3b5576795e3a0b664675
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-01 08:38:00 -0800

    say

---
+++ say/test.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+
+class Say(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    async def say(self, ctx, *, message):
+        await ctx.send(message)
+        await bot.delete(ctx.message)
+
+def setup(bot):
+    bot.add_cog(Say(bot))
