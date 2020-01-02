122e917aaf4f1d82dbf1f3eebd5f0998b44b22b9
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-02 07:58:58 -0800

    Create hastebin.py

---
+++ hastebin/hastebin.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import os
+
+class Hastebin(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+    @commands.command(aliases = 'haste')
+    async def hastebin(self, ctx, *, text):
+    	"""Puts text in hastebin"""
+    	async with aiohttp!.ClientSession() as session:
+    		async with session.post("https://hastebin/documents", data=text) as response:
+    			json = await response.json
+    			await ctx.send(f"https://hastebin.com/{json['key']}")
+
+def setup(bot):
+	bot.add_cog(Hastebin(Bot))
