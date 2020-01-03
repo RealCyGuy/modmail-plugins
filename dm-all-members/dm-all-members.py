4d9f68fd2f6ffd96b94b0a16935d709bc800c199
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-03 00:00:49 -0800

    Create dm-all-members.py

---
+++ dm-all-members/dm-all-members.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+class dm-all-members(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def dmall(self, ctx, *, message):
+        """
+        Makes ModMail dm all members in the server.
+
+        Usage: [prefix]dmall <message>
+        """
+        for x in message.guild.members:
+            asyncio.sleep(1)
+            membrr = x
+            await membrr.send(message)
+
+
+
+def setup(bot):
+    bot.add_cog(dm-all-members(bot))
