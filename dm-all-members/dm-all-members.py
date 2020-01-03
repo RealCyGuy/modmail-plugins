714d786799d727d380bb9eaeed62b0a85eb5e2bb
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-03 00:02:13 -0800

    Update dm-all-members.py

--- dm-all-members/dm-all-members.py
+++ dm-all-members/dm-all-members.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 
-class dm-all-members(commands.Cog):
+class dmallmembers(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -21,4 +21,4 @@ class dm-all-members(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(dm-all-members(bot))
+    bot.add_cog(dmallmembers(bot))
