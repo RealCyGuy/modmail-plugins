caf7a38c43b56dd51a93c26962babd6a578e6b24
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-07 18:20:59 -0800

    new plugin!

---
+++ wikipedia/wikipedia.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import wikipedia
+
+class Wikipedia(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def wikipedia(self, ctx, *, search):
+        """Searches on the english Wikipedia"""
+        results = wikipedia.search(search)
+
+        for result in results:
+        try:
+            page = wikipedia.page(result)
+        except wikipedia.exceptions.DisambiguationError:
+            ctx.send('DisambiguationError')
+            continue
+        except wikipedia.exceptions.PageError:
+            ctx.send(f'PageError for result: {result}')
+            continue
+
+        ctx.send(page.summary.encode('utf-8'))
