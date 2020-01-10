b839e57716665db4dffc51959cd4282e12ddec42
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-10 15:51:52 -0800

    Update wikipedia.py

--- wikipedia/wikipedia.py
+++ wikipedia/wikipedia.py
@@ -9,7 +9,19 @@ class Wikipedia(commands.Cog):
     @commands.command()
     async def wikipedia(self, ctx, *, search):
         """Searches on the english Wikipedia"""
-        ctx.send(wikipedia.summary(search, sentences=1))
+        results = wikipedia.search(search)
+
+        for result in results:
+            try:
+                page = wikipedia.page(result)
+            except wikipedia.exceptions.DisambiguationError:
+                ctx.send('DisambiguationError')
+                continue
+            except wikipedia.exceptions.PageError:
+                ctx.send('PageError for result: ' + result)
+                continue
+
+            print(page.summary.encode('utf-8'))
 
 def setup(bot):
     bot.add_cog(Wikipedia(bot))
