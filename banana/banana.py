6cd0a9f770afeff164a883f82afaedcd57911b2f
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-14 16:55:15 -0800

    Create banana.py

---
+++ banana/banana.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Banana(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(message):
+        if 'banana' in message:
+            await message.channel.send('hi')
+
+def setup(bot):
+    bot.add_cog(Banana(bot))
