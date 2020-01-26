974a3c5d53a46ac327e7598abae3f0a4a82b9128
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-25 22:58:10 -0800

    reacts with 'ping'

--- react-on-ping/react-on-ping.py
+++ react-on-ping/react-on-ping.py
@@ -3,14 +3,21 @@ from discord.ext import commands
 
 class ReactOnPing(commands.Cog):
     """Reacts with a ping emoji when someone gets pinged."""
+    emojis = [
+    "\N{REGIONAL INDICATOR LETTER P}",
+    "\N{REGIONAL INDICATOR LETTER I}",
+    "\N{REGIONAL INDICATOR LETTER N}",
+    "\N{REGIONAL INDICATOR LETTER G}"
+    ]
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if '<@' in message.content.upper():
-            # await message.add_reaction(emoji)
-            await message.channel.send(list(self.bot.get_all_emojis()))
+            for emoji in self.emojis:
+                await message.add_reaction(emoji)
 
 def setup(bot):
     bot.add_cog(ReactOnPing(bot))
