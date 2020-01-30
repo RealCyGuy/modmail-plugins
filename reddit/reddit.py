64ec7372448c7efcc7a2cfa581d883838a8f4f07
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-30 07:07:24 -0800

    reddit scroller

---
+++ reddit/reddit.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+import requests
+
+from box import Box
+
+from core.paginator import EmbedPaginatorSession
+
+class RedditScroller(commands.Cog):
+    """See cursed images"""
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def cursedimages(self, ctx):
+        """Scroll through cursed images."""
+        r = requests.get("https://api.reddit.com/r/cursedimages/top.json?sort=top&t=day&limit=10", headers={'User Agent': 'Super Bot 9000'})
+        r = r.json()
+        r = Box(r)
+
+        embeds = []
+
+        for data in r.data.children.data:
+            ctx.send(data)
+
+def setup(bot):
+    bot.add_cog(RedditScroller(bot))
