4302d71073c2415afa2db4291aca1758f3ca2efe
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-30 18:54:41 -0800

    moved

--- cursedimages/cursedimages.py
+++ cursedimages/cursedimages.py
@@ -7,14 +7,14 @@ from box import Box
 
 from core.paginator import EmbedPaginatorSession
 
-class RedditScroller(commands.Cog):
+class CursedImages(commands.Cog):
     """See cursed images"""
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def cursedimages(self, ctx):
-        """Scroll through cursed images. :)"""
+        """Scroll through cursed images."""
         subreddit = "cursedimages"
         r = requests.get(f"https://api.reddit.com/r/{subreddit}/top.json?sort=top&t=day&limit=10",
                              headers={'User-agent': 'Super Bot 9000'})
@@ -25,7 +25,12 @@ class RedditScroller(commands.Cog):
         embeds = []
 
         for data in boxed.data.children.data:
-            ctx.send(data)
+            title = data.title
+            image = data.url
+
+            embed = discord.Embed(title=title, color=0x22ddbbff)
+            embed.set_image(image)
+            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
 
 def setup(bot):
-    bot.add_cog(RedditScroller(bot))
+    bot.add_cog(CursedImages(bot))
