34e12ff5907099458b359cafd248076bd73531b8
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-30 18:35:19 -0800

    Update reddit.py

--- reddit/reddit.py
+++ reddit/reddit.py
@@ -17,7 +17,7 @@ class RedditScroller(commands.Cog):
         """Scroll through cursed images."""
         subreddit = "cursedimages"
         r = requests.get(f"https://api.reddit.com/r/{subreddit}/top.json?sort=top&t=day&limit=10",
-                     headers={'User Agent': 'Super Bot 9000'}).json()
+                             headers={'User-agent': 'Super Bot 9000'})
         ctx.send(r)
         boxed = Box(r)
 
@@ -25,21 +25,6 @@ class RedditScroller(commands.Cog):
 
         for data in boxed.data.children.data:
             ctx.send(data)
-
-    @commands.command()
-    async def paginator(self, ctx):
-        embeds = []
-        pages = ['I like watermelon', 'Do you?', 'I do!']
-        for page in pages:
-            embed = discord.Embed(
-                title="Watermelon",
-                color=self.bot.main_color,
-                description=page,
-                )
-            embeds.append(embed)
-
-        session = EmbedPaginatorSession(ctx, *embeds)
-        await session.run()
 
 def setup(bot):
     bot.add_cog(RedditScroller(bot))
