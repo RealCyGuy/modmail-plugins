a08478e0e4f4d9b4fbb63dfd34713c7d362e79b9
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-02-11 21:38:30 -0800

    urban dictionary search plugin

---
+++ urban/urban.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+import requests
+
+from box import Box
+
+from core.models import EmbedPaginatorSession
+
+class UrbanDictionary(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def urban(self, ctx, *, search):
+        r = requests.get(f"https://api.urbandictionary.com/v0/define?term={search}",
+                             headers={'User-agent': 'Super Bot 9000'})
+        r = r.json()
+        data = Box(r)
+
+        if not data.list:
+            embed = discord.Embed()
+            embed.color = self.bot.error_color
+            embed.title = "There is nothing here, try again."
+            await ctx.send(embed=embed)
+        else:
+            for definition in data.list:
+
+
+def setup(bot):
+    bot.add_cog(UrbanDictionary(bot))
