c35a7be62a645b67fcd97e97657d4c5c094bbc5e
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-02-27 17:58:21 -0800

    It does stuff now :)

--- pokecord/pokecord.py
+++ pokecord/pokecord.py
@@ -6,6 +6,8 @@ logger = logging.getLogger("Modmail")
 
 import hashlib
 import requests
+import json
+import os
 
 class Pokecord(commands.Cog):
     """
@@ -20,9 +22,20 @@ class Pokecord(commands.Cog):
         if message.author.id == 365975655608745985 and message.embeds:
             if message.embeds[0].title == "‌‌A wild pokémon has аppeаred!":
                 image = requests.get(message.embeds[0].image.url).content
-                with open("pokemon.jpg", 'wb') as f:
+                with open(os.path.join(os.path.dirname(__file__), "hashes.json"), "r") as f:
+                    hashes = json.load(f)
+
+                with open(os.path.join(os.path.dirname(__file__), "pokemon.jpg"), 'wb') as f:
                     f.write(image)
-    
+
+                with open(os.path.join(os.path.dirname(__file__), "pokemon.jpg"), "rb") as f:
+                    md = hashlib.md5(f.read()).hexdigest()
+
+                try:
+                    pokemon = hashes[md]
+                    await message.channel.send(f"My magical guessing powers guess it's a... {pokemon}.")
+                except KeyError:
+                    logger.info(f"Pokemon {md} not found.")
 
 def setup(bot):
     bot.add_cog(Pokecord(bot))
