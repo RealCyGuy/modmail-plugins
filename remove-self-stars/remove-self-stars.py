a4f8d601deb1a820731ee65e643b54d2cdfb152a
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-02-29 08:40:41 -0800

    Update remove-self-stars.py

--- remove-self-stars/remove-self-stars.py
+++ remove-self-stars/remove-self-stars.py
@@ -14,18 +14,20 @@ class RemoveSelfStars(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
+        server = self.bot.get_guild(payload.guild_id)
+        member = server.get_member(payload.user_id)
         try:
-            message = await payload.member.fetch_message(payload.message_id)
+            message = await member.fetch_message(payload.message_id)
             if payload.emoji == "⭐" and payload.user_id == message.author.id:
                 try:
                     await message.remove_reaction(payload.member, "⭐")
                 except discord.Forbidden:
                     logger.error(
-                        f"I didn't have permissions to remove a self star from {payload.member.name}#{pauload.member.discriminator}."
+                        f"I didn't have permissions to remove a self star from {payload.member.name}#{payload.member.discriminator}."
                     )
                 except Exception as e:
                     logger.error(e)
-        except discord.NotFound:  # Not a self-star.
+        except discord.NotFound:
             pass
 
 
