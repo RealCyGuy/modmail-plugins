7ba9d763dc02c43f5ce1218918673d06a694f5f3
aniket091 <75978424+aniket091@users.noreply.github.com>
2021-01-06 14:32:20 +0530

    Update suggest.py

--- suggest/suggest.py
+++ suggest/suggest.py
@@ -59,7 +59,9 @@ class Suggest(commands.Cog):
                     embed.set_author(
                         name=f"Suggestion by {ctx.author}:", icon_url=ctx.author.avatar_url
                     )
-                    await suggestion_channel.send(embed=embed)
+                    message_ = await suggestion_channel.send(embed=embed)
+                    await message_.add_reaction("⬆️")
+                    await message_.add_reaction("⬇️")
                     await ctx.message.add_reaction("\N{WHITE HEAVY CHECK MARK}")
         else:
             await ctx.send(embed=discord.Embed(color=self.bot.error_color, title=f"You have been blocked, {ctx.author.name}#{ctx.author.discriminator}.", description=f"Reason: {self.banlist[str(ctx.author.id)]}"))
