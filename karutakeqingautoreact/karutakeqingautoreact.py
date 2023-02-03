e6fdd675aed01bad1806dfced37b71600969cffa
Cyrus Yip <cyruscmyip1@gmail.com>
2023-02-02 23:05:35 -0800

    add more reactions to karutakeqingautoreact.py

--- karutakeqingautoreact/karutakeqingautoreact.py
+++ karutakeqingautoreact/karutakeqingautoreact.py
@@ -1,10 +1,25 @@
+import unicodedata
+from typing import List
+
 import discord
 from discord.ext import commands
 
 
+async def add_reactions(message: discord.Message, reactions: List[str]) -> None:
+    for reaction in reactions:
+        try:
+            emoji = unicodedata.lookup(reaction)
+        except KeyError:
+            emoji = reaction
+        try:
+            await message.add_reaction(emoji)
+        except discord.Forbidden:
+            break
+
+
 class KarutaKeqingAutoReact(commands.Cog):
     """
-    Reacts to karuta character lookup and collection for keqing bot.
+    Automatically add reactions to Karuta messages for Keqing Bot that usually requires premium.
     """
 
     def __init__(self, bot):
@@ -15,10 +30,12 @@ class KarutaKeqingAutoReact(commands.Cog):
         if message.author.id == 646937666251915264 and message.embeds:
             embed = message.embeds[0]
             if embed.title.startswith("Card Collection"):
-                await message.add_reaction("📝")
-            elif embed.title.startswith("Character Lookup"):
-                await message.add_reaction("🖌️")
+                await add_reactions(message, ["Left-Pointing Magnifying Glass", "Memo"])
+            elif embed.title in ["Character Lookup", "Character Results"]:
+                await add_reactions(message, ["🖌️", "Money Bag", "Revolving Hearts", "🎖️"])
+            elif embed.title.startswith("Bits"):
+                await add_reactions(message, ["Heavy Plus Sign"])
 
 
-def setup(bot):
-    bot.add_cog(KarutaKeqingAutoReact(bot))
+async def setup(bot):
+    await bot.add_cog(KarutaKeqingAutoReact(bot))
